893c9ce90d6dc2eb9e4141de701c61dcf90e5dac
Doidoria <th15926@daum.net>
2026-07-06 11:12:50 +0900

    7.6

--- BN/src/main/java/com/yescare/api/ApiApplication.java
+++ BN/src/main/java/com/yescare/api/ApiApplication.java
@@ -4,10 +4,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.TimeZone;
 
+@EnableRetry
 @EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
